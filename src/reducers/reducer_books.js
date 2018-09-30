678edfa9af6a4e54be0016f6be662bded3601447
John Herman Witham <john.h.witham@gmail.com>
2018-09-30 03:37:18 +0300

    Add books reducer & export func which return book titles

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Toyota Way' },
+    { title: 'Running' },
+    { title: 'Grinding It' },
+    { title: 'From Nowhere to Somewhere' },
+    { title: 'How to American' }
+  ]
+}
